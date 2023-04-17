0fb39251cce2ca9638467895eea9d65087758524
Tsvetkova Nina <tsvnishka@gmail.com>
2023-04-17 21:47:48 +0300

    Add EmployController Add EmployeeService Add class Employee Add Exeption Add ResponseStatus for Exeption

---
+++ src/main/java/sky/pro/java/collectionsandsets/controller/EmployeeControllerNew.java
@@ -0,0 +1,44 @@
+package sky.pro.java.collectionsandsets.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sky.pro.java.collectionsandsets.Employee;
+import sky.pro.java.collectionsandsets.service.EmployServiceNew;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/departments")
+public class EmployeeControllerNew {
+
+    private final EmployServiceNew employeeService;
+
+    public EmployeeControllerNew(EmployServiceNew employeeService) {
+
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/max-salary")
+    public Employee getEmployWithMaxSalaryByDept(@RequestParam("departmentId") int dept) {
+        return employeeService.getEmployWithMaxSalaryByDept(dept);
+    }
+
+    @GetMapping("/min-salary")
+    public Employee getEmployWithMinSalaryByDept(@RequestParam("departmentId") int dept) {
+        return employeeService.getEmployWithMinSalaryByDept(dept);
+    }
+
+    @GetMapping(value = "/all", params = {"departmentId"})
+    public List<Employee> getInfoAllEmployeeByDept(@RequestParam(value = "departmentId") int dept) {
+        return employeeService.getInfoAllEmployeeByDept(dept);
+    }
+
+
+    @GetMapping(value = "/all")
+    public Map<Integer, List<Employee>> printEmployeesByDepartment() {
+        return employeeService.printEmployeesByDepartment();
+    }
+}
